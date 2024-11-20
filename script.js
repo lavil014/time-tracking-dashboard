0bf18da61b0cbe2516dbf1b66bb52bfb6a01afc6
Luis Aviles <123853326+lavil014@users.noreply.github.com>
2024-11-19 23:22:31 -0600

    New changes made to my script.js file with function to render cards with data

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const btns = document.querySelectorAll('button');
-const dailyData = document.getElementById('daily');
-const weeklyData = document.getElementById('weekly');
-const monthlyData = document.getElementById('monthly');
 const cardContainer = document.querySelector('.card-container')
 
 
-/*
+
+const renderCards = (data, timeFrame)=>{
+    
+  cardContainer.innerHTML = '';
+
+  data.forEach(({title,timeframes}) => {
+       
+      cardContainer.innerHTML +=
+        `
+          <div class="card">
+            <span>${title}</span><br>
+            <span>${timeframes[timeFrame].current}hrs</span><br>
+            <span>Last Week - ${timeframes[timeFrame].previous}hrs</span>  
+          </div>
+        `
+  });
+}
+
+
+
 const fetchingData = async ()=>{
 
     try{
@@ -13,21 +29,13 @@ const fetchingData = async ()=>{
 
         let response = await data.json();
 
+          btns.forEach(btnElement => {
+            btnElement.addEventListener('click',(e)=>{
+              let timeFrame = e.target.id;
 
-        response.forEach(element => {
-
-          const {title, timeframes, daily, weeky, monthly} = element;
-
-            cardContainer.innerHTML += 
-            `
-              <div class="card">
-                <span>${title}</span><br>
-                <span>${timeframes.daily.current}hrs</span><br>
-                <span>Last Week - ${timeframes.daily.previous}hrs</span>  
-              </div>
-            `
-        });
-
+              renderCards(response, timeFrame);
+          })
+});
     }
     catch(error){
         console.error('Error fetching data :' , error);
@@ -36,19 +44,4 @@ const fetchingData = async ()=>{
 }
 
 fetchingData()
-*/
-const showData = ()=>{
 
-}
-
-btns.forEach(element => {
-  element.addEventListener('click',(e)=>{
-    if(e.target.id === 'daily'){
-      console.log('Daily btn was clicked');
-    } else if(e.target.id === 'weekly'){
-      console.log('Weekly btn was clicked');
-    } else{
-      console.log('Monthly btn was clicked');
-    }
-  })
-});
